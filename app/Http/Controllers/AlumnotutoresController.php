d73b381c01333d7476d915a2ad278ce12aa3275b
Rafael Aleajndro <rafael.rmartinez@alumnos.udg.mx>
2023-11-24 22:57:55 -0600

    Primera version

---
+++ app/Http/Controllers/AlumnotutoresController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alumnotutores;
+use Illuminate\Http\Request;
+
+class AlumnotutoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Alumnotutores  $alumnotutores
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Alumnotutores $alumnotutores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Alumnotutores  $alumnotutores
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Alumnotutores $alumnotutores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Alumnotutores  $alumnotutores
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Alumnotutores $alumnotutores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Alumnotutores  $alumnotutores
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Alumnotutores $alumnotutores)
+    {
+        //
+    }
+}
